63a903330aee94f2bc6ecdf082140df088882f1a
Edwin Pajuelo <edwin.934876433@gmail.com>
2022-07-15 18:11:03 -0500

    first commit

---
+++ Config/Config - copia1.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL = "http://ftpupload.net/PexTumuy_Oficial/";
+    const HOST = "ftpupload.net";
+    const BD = "epiz_32162017";
+    const DB_USER = "epiz_32162017";
+    const PASS = "pbJFsL1xqVrXujj";
+    const CHARSET = "charset=utf8";
+?>
---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL = "http://localhost/PexTumuy_Oficial/";
+    const HOST = "localhost";
+    const BD = "pextumuy";
+    const DB_USER = "root";
+    const PASS = "root";
+    const CHARSET = "charset=utf8";
+?>
